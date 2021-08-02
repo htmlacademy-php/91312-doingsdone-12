566a1bb9c7ade614fb62ff4992eba5af49d9508b
ArtyomHachunts <a.hachunc@gmail.com>
2021-08-02 23:01:44 +0300

    module3-task2 'по мотивам всех правок и замечаний'

--- templates/layout.php
+++ templates/layout.php
@@ -10,8 +10,9 @@
 </head>
 
 <body>
-<h1 class="visually-hidden">$title</h1>
+<h1 class="visually-hidden"><?php $title ?></h1>
 <div class="page-wrapper">
+
     <div class="container container--with-sidebar">
         <header class="main-header">
             <a href="/">
@@ -19,7 +20,8 @@
             </a>
 
             <div class="main-header__side">
-                <a class="main-header__side-item button button--plus open-modal" href="pages/form-task.html">Добавить задачу</a>
+                <a class="main-header__side-item button button--plus open-modal" href="pages/form-task.html">Добавить
+                    задачу</a>
 
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__data">
--- templates/main.php
+++ templates/main.php
@@ -1,15 +1,23 @@
+<?php
+/**
+ * @var $show_complete_tasks
+ * @var $tasks
+ * @var $doings
+ */
+?>
 <div class="content">
     <section class="content__side">
         <h2 class="content__side-heading">Проекты</h2>
 
         <nav class="main-navigation">
             <ul class="main-navigation__list">
-                <?php  foreach ($tasks as $task): ?>
+                <?php foreach ($tasks as $task): ?>
                     <li class="main-navigation__list-item">
-                    <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($task); ?></a>
-                    <span class="main-navigation__list-item-count"><?= сategoryCount($doings, $task) ?></span>
+                        <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($task); ?></a>
+                        <span class="main-navigation__list-item-count"><?= сategoryCount($doings, $task) ?></span>
                     </li>
-                <?php endforeach;?>
+                <?php endforeach; ?>
+
             </ul>
         </nav>
         <a class="button button--transparent button--plus content__side-button"
@@ -36,44 +44,28 @@
             <label class="checkbox">
                 <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
                 <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1): ?> checked <?php endif; ?> >
-                <span class="checkbox__text">Показывать выполненные</span>
+            <span class="checkbox__text">Показывать выполненные</span>
             </label>
         </div>
- <table class="tasks">
-<?php
-foreach ($doings as $doing) {
-if ($doing['status'] === true && $show_complete_tasks == 0) {
-continue;
-}
-?>
-<tr class="tasks__item task <? echo ($doing['status'] == true && $show_complete_tasks == 1) ? 'task--completed' : ''  ?>
-<? echo ($doing['status'] !== true && $doing['date'] !== null && get_date_difference($task['date']) <= $quantity_hours_in_day) ? 'task--important' : '' ?>">
-<td class="task__select">
-<label class="checkbox task__checkbox">
-<input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
-<span class="checkbox__text"><?= htmlspecialchars($doing['task']); ?></span>
-</label>
-</td>
-<td class="task__file">
-<a class="download-link" href="#">Home.psd</a>
-</td>
-<td class="task__date"><?= htmlspecialchars($doing['date']); ?></td>
-</tr>
-<?php } ?> <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
-            <?php if ($show_complete_tasks == 1): ?>
-                <tr class="tasks__item task task--completed">
-                    <td class="task__select">
+        <table class="tasks">
+            <?php foreach ($doings as $doing): ?>
+            <?php if ($doing['status'] && !$show_complete_tasks) { continue; } ?>
+            <?php $taskCompletedClass = ($doing['status'] && $show_complete_tasks) ? 'task--completed' : '' ?>
+            <?php $taskImportantClass = (!$doing['status'] && $doing['is_important']) ? 'task--important' : '' ?>
+                <tr class="tasks__item task <?= $taskCompletedClass ?> <?= $taskImportantClass ?>">
+                <td class="task__select">
                         <label class="checkbox task__checkbox">
-                            <input class="checkbox__input visually-hidden" type="checkbox" checked>
-                            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                            <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" value="1">
+                            <span class="checkbox__text"><?= htmlspecialchars($doing['task']); ?></span>
                         </label>
                     </td>
-                    <td class="task__date">10.10.2019</td>
-                    <td class="task__controls"></td>
+                    <td class="task__file">
+                        <a class="download-link" href="#">Home.psd</a>
+                    </td>
+                    <td class="task__date"><?= htmlspecialchars($doing['date']); ?></td>
                 </tr>
- </table>
-
-        <?php endif; ?>
+            <?php endforeach ?>
+        </table>
     </main>
 </div>
 
