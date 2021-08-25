f61c20d5913fbb8bb4d7f08e03e2afcf11a1ac80
ArtyomHachunts <a.hachunc@gmail.com>
2021-08-25 23:44:46 +0300

    added indexes

--- schema.sql
+++ schema.sql
@@ -5,10 +5,9 @@ CREATE DATABASE doingsdone_db
 USE doingsdone_db;
 
 CREATE TABLE users (
-    email VARCHAR(100) NOT NULL,
     CREATE UNIQUE INDEX uk_email (email),
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(128) NOT NULL UNIQUE,
+    name VARCHAR(128) NOT NULL,
     email VARCHAR(128) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -28,13 +27,20 @@ CREATE TABLE tasks (
     email VARCHAR(100) NOT NULL,
     CREATE UNIQUE INDEX uk_email (email),
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
+    title VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_deadline TIMESTAMP,
     status BIT(1) DEFAULT 0 NOT NULL,
     project_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
 
 );
+
+CREATE INDEX search_by_name
+ON users (name);
+CREATE INDEX search_by_projects
+ON projects (title);
+CREATE INDEX search_by_tasks
+ON tasks (title);
