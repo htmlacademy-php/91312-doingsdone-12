386446f40532d1ad0f731b6704c0540a7c766327
ArtyomHachunts <a.hachunc@gmail.com>
2021-08-26 23:28:50 +0300

    one more edit

--- schema.sql
+++ schema.sql
@@ -1,43 +1,40 @@
 CREATE DATABASE doingsdone_db
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
-
 USE doingsdone_db;
-
 CREATE TABLE users (
     CREATE UNIQUE INDEX uk_email (email),
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
     email VARCHAR(128) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
 );
-
 CREATE TABLE projects (
     id  INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL UNIQUE,
     user_id INT NOT NULL,
+    -- Here, I am trying to enforce point 5.1 "Связи: автор: пользователь, создавший проект;"
+    FOREIGN KEY (user_id) REFERENCES users(name),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
 );
-
 CREATE TABLE tasks (
-    email VARCHAR(100) NOT NULL,
-    CREATE UNIQUE INDEX uk_email (email),
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
-    date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_deadline TIMESTAMP,
+    -- Here, I am trying to enforce point 5.2 from SRS (Связи:автор: пользователь, создавший задачу;)
+    FOREIGN KEY (user_id) REFERENCES users(name),
+    deadline_date TIMESTAMP,
     status BIT(1) DEFAULT 0 NOT NULL,
     project_id INT NOT NULL,
+    -- Here, I am trying to enforce point 5.2 from SRS (проект: проект, которому принадлежит задача.)
+    FOREIGN KEY (project_id) REFERENCES projects(title),
+    file_url VARCHAR(2048) NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
-
 );
-
 CREATE INDEX search_by_name
 ON users (name);
 CREATE INDEX search_by_projects
