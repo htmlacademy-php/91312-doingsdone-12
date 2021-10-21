d31b498ec6f897e8357fce08296c1abc32d1f84e
Артём <a.hachunc@gmail.com>
2021-10-21 15:32:34 +0300

    исправления от 21.20.2021

--- queries.sql
+++ queries.sql
@@ -1,18 +1,20 @@
-INSERT INTO users (email, title, password, created_at) 
+INSERT INTO users (email, name, password, created_at) 
 VALUES ('a.hachunc@gmail.com', 'Артём', '1234', '2021-10-17 22:57:12'),
 ('htmlacademy@gmail.com','Кот','54321','2021-10-17 22:57:12');
-INSERT INTO projects (id, title) 
-VALUES (1, 'Входящие'), (1, 'Учеба'), 
-(1, 'Работа'), (1, 'Домашние дела'), (1, 'Авто');
-INSERT INTO tasks (id, title, title, created_at, updated_at, status)
-VALUES (1, 3, 'Собеседование в IT компании', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
-       (1, 3, 'Выполнить тестовое задание', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
-       (1, 2, 'Сделать задание первого раздела', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
-       (1, 1, 'Встреча с другом', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
-       (1, 4, 'Купить корм для кота', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
-       (1, 4, 'Заказать пиццу', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0);
 
-SELECT * FROM projects WHERE id = 1;
-SELECT * FROM tasks WHERE title = 3;
+INSERT INTO projects (title) VALUES ('Входящие'), ('Учеба'), ('Работа'), ('Домашние дела'), ('Авто');
+
+INSERT INTO tasks (title, created_at, updated_at, status)
+VALUES ('Собеседование в IT компании', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
+       ('Выполнить тестовое задание', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
+       ('Сделать задание первого раздела', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
+       ('Встреча с другом', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
+       ('Купить корм для кота', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0),
+       ('Заказать пиццу', '2021-08-03 23:59:00', '2021-08-03 23:59:00', 0);
+
+SELECT id, title, user_id, created_at, updated_at 
+FROM projects WHERE id = 1;
+SELECT id, title, deadline_date, project_id, file_url, created_at, updated_at 
+FROM tasks WHERE title = 3;
 UPDATE tasks SET status='1' WHERE id = 1;
 UPDATE tasks SET title = 'Сделать задачу' WHERE id = 1;
--- schema.sql
+++ schema.sql
@@ -1,36 +1,32 @@
+DROP DATABASE IF EXISTS doingsdone_db;
 CREATE DATABASE doingsdone_db
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 USE doingsdone_db;
 CREATE TABLE users (
-    CREATE INDEX (128) uk_email (email),
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(128) NOT NULL,
     email VARCHAR(128) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
+    password CHAR(60) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME
 );
 CREATE TABLE projects (
-    id  INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    user_id INT NOT NULL,
-    -- Here, I am trying to enforce point 5.1 "Связи: автор: пользователь, создавший проект;"
-    FOREIGN KEY (user_id) REFERENCES users(name),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    created_at DATETIME,
+    updated_at DATETIME
 );
 CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    -- Here, I am trying to enforce point 5.2 from SRS (Связи:автор: пользователь, создавший задачу;)
-    FOREIGN KEY (user_id) REFERENCES users(name),
-    deadline_date TIMESTAMP,
+    title VARCHAR(1024) NOT NULL,
+    deadline_date DATETIME,
     status BIT(1) DEFAULT 0 NOT NULL,
-    project_id INT NOT NULL,
-    -- Here, I am trying to enforce point 5.2 from SRS (проект: проект, которому принадлежит задача.)
-    FOREIGN KEY (project_id) REFERENCES projects(title),
+    project_id INT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id),
     file_url VARCHAR(2048) NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE now()
+    created_at DATETIME,
+    updated_at DATETIME
 );
